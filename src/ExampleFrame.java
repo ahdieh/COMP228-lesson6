b4254b4c8100bedebcc211f0cc9e755daa689b74
ahdieh <aemadi@my.centennialcollege.ca>
2015-10-25 13:04:49 -0500

    Modified the program and the ExampleFrame to create the First Name lable

--- src/ExampleFrame.java
+++ src/ExampleFrame.java
@@ -4,21 +4,35 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
 public class ExampleFrame extends JFrame {
 // PRIVATE INSTANCE VARIABLES
 	private JPanel _contentPanel;
-
+    private EmptyBorder _myEmptyBoarder;
+    private BorderLayout _myBorderLayout;
+    private JLabel _firstLable;
 	
 	
 // CONSTRUCTOR +++++++++++++++++++++++++++
 	public ExampleFrame() {
+		this._initialize();
+	}
+
+
+//PRIVATE METHOD
+	private void _initialize() {
+		this._myEmptyBoarder = new EmptyBorder(5, 5, 5, 5);
+		this._myBorderLayout = new BorderLayout(0, 0);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		this._contentPanel = new JPanel();
-		this._contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this._contentPanel.setLayout(new BorderLayout(0, 0));
+		this._contentPanel.setBorder(this._myEmptyBoarder );
 		setContentPane(this._contentPanel);
+		_contentPanel.setLayout(this._myBorderLayout);
+		
+		this._firstLable = new JLabel("First Name:");
+		this._contentPanel.add(this._firstLable , BorderLayout.NORTH);
 	}
 
 }
--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,29 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 // DRIVER CLASS 
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("jframe");
-		ExampleFrame frame = new ExampleFrame();
-		frame.setVisible(true);
+		
+		ExampleFrame myframe = new ExampleFrame();
+		myframe.setVisible(true);
+		
+	//JFrame myFrame = new JFrame();
+	//JPanel myPanel = new JPanel();
+	
+	//myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//myFrame.setBounds(100, 100, 450, 300);
+	
+	//myPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+	//myPanel.setLayout(new BorderLayout(0, 0));
+	
+	//myFrame.setContentPane(myPanel);
+	//myFrame.setVisible(true);
+
 	}
 
 }
